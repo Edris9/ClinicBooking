cc9b3a5da69ebd01aca6bef8bff7f5a15972d65d
Edris9 <Edriskohestani1010@gmail.com>
2026-05-11 16:23:35 +0200

    felet på att spara på objekt i repository fixades med att lägga några mer metoder för att spara, tabort, och uppdatera

--- ClinicBooking.Infrastructure/Repositories/GenericRepository.cs
+++ ClinicBooking.Infrastructure/Repositories/GenericRepository.cs
@@ -17,15 +17,22 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
     public async Task<T> GetByIdAsync(int id) =>
         await _context.Set<T>().FindAsync(id);
 
-    public async Task AddAsync(T entity) =>
+    public async Task AddAsync(T entity)
+    {
         await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
 
-    public async Task UpdateAsync(T entity) =>
+    public async Task UpdateAsync(T entity)
+    {
         _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
 
     public async Task DeleteAsync(int id)
     {
         var entity = await GetByIdAsync(id);
         _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
     }
 }
