3b036caa1a3a19124990ec2cb1c4a33eb70a23d8
Edris9 <Edriskohestani1010@gmail.com>
2026-09-26 01:58:14 +0200

    created table and buttoms för the objects !

--- ClinicBooking.Client/Models/CreateDoctorDto.cs
+++ ClinicBooking.Client/Models/CreateDoctorDto.cs
@@ -6,6 +6,6 @@
         public string Specialty { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public int DepartmentId { get; set; }
+        public int DepartmentId { get; set; } = 0;
     }
 }
